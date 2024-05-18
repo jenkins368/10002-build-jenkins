7ac95dd1630d3f82d471233e64570770f28a05ee
gnuhub <gnuhub@qq.com>
2024-05-18 15:07:59 +0800

    build jenkins

--- 1.ci.sh
+++ 1.ci.sh
@@ -11,5 +11,23 @@ git clone --depth 1 https://github.com/jenkinsci/jenkins.git
 cd jenkins
 ls -al
 
-mvn  install  -Dmaven.test.skip=true
+mvn -am -pl war,bom -Pquick-build clean install
+
 ls -al
+ 
+
+# ./gradlew localDistro
+# ./gradlew :distribution:archives:linux-tar:assembl
+
+# tree -L 3
+# ls -al build/distribution/local/
+
+# cd build/distribution/local/
+
+# tar cf elasticsearch-8.13.4-SNAPSHOT.tar elasticsearch-8.13.4-SNAPSHOT
+# ls -al  
+# file elasticsearch-8.13.4-SNAPSHOT.tar
+# cp elasticsearch-8.13.4-SNAPSHOT.tar ~/
+# cd ~/elasticsearch
+# file build/distribution/local/elasticsearch-8.13.4-SNAPSHOT.tar
+# file ~/elasticsearch-8.13.4-SNAPSHOT.tar
